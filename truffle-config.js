4407093481d1f6ad02c8fc31f7059829b22204b8
Nikit Swaraj <nikitsrj@gmail.com>
2023-12-24 21:26:20 +0800

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -65,8 +65,8 @@ module.exports = {
     // options below to some value.
     //
     development: {
-     host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
+     host: "54.161.63.247",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
     //
